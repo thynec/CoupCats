579cd2710333327a70e616d3724c71ec7e76c939
kademarcus <kmsp240@uky.edu>
2025-02-25 22:45:46 -0500

    Update starter_regressions.R
    added cooks D and base plots, as well as notes under the libraries for class or for anyone on team to look at beforehand

--- starter_regressions.R
+++ starter_regressions.R
@@ -7,7 +7,6 @@ install.packages('broom')
 install.packages("httr")
 install.packages("readr")
 install.packages("margins")
-
 #libraries
 library(aod)
 library(ggplot2)
@@ -17,7 +16,13 @@ library(broom)
 library(httr)
 library(readr)
 library(margins)
-
+'Notes: 1. Need to look at z scores of marginal effects for both models
+2. Need to graph predicted probability change for each variable (for example, change in probability
+of coup_attempted for median age. Need plots for each individual variable)
+3. Need to make output tables that dont just show up in R console (for original model and marginal effects)
+4. Need to change plots for both models residuals, I believe it has something to do with the number of observations
+changing from the fake data set. See if plotting this is reasonable or if we need to change something (probably)
+5. Cooks D needs labels for the observation points'
 # URL for the raw file
 url <- "https://raw.githubusercontent.com/seque1204/TEK300/main/emma_data.xlsx"
 
@@ -32,14 +37,15 @@ coup_data <- read_xlsx(temp_file)
 head(coup_data)
 
 
-#logit with coup as dv and gdp + military expenditure + media_freedom
+#logit with coup as dv and age/population variables 
 mylogit <- glm(`coup_attempted` ~ `popln_tot` + `age0_14` + `age15_64` + `age65plus` + `median_age`, data = coup_data, family = 'binomial')
 summary(mylogit)#R logit output
 
 #probability change
 predicted_logit <- predict(mylogit, type = 'response')
-marginal_effects <- margins(mylogit)
-summary(marginal_effects)
+marginal_effectsL <- margins(mylogit)
+summary(marginal_effectsL) # R rounds to 0
+print(marginal_effectsL, digits = 6) # do this to see actual values
 
 #residuals
 logit_residuals <- residuals(mylogit)
@@ -58,15 +64,24 @@ ggplot(coup_data, aes(x = 1:249, y = pearson_residuals_logit)) +
        x = "Observations", y = "Pearson Residuals") +
   theme_minimal()
 
+#find Cook's Distance (for influential outliers)
+logit_cooksD = cooks.distance(mylogit)
+
+#plotting Cook's Distance (base, need to add clarity and labels)
+plot(logit_cooksD, type = "p", main = "Cook's Distance (Logit)", ylab = "Cook's Distance", xlab = "Observation Index")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 #probit with same variables 
-myprobit <- glm(`coup_attempted` ~ `ccode` + `popln_tot` + `age0_14` + `age15_64` + `age65plus` + `median_age`, data = coup_data, family = binomial(link = 'probit'))
+myprobit <- glm(`coup_attempted` ~`popln_tot` + `age0_14` + `age15_64` + `age65plus` + `median_age`, data = coup_data, family = binomial(link = 'probit'))
 summary(myprobit)
 
 
 #predicted probabilities for probit
-predicted_probit <- predict(mylogit, type = 'response')
-marginal_effects <- margins(mylogit)
-summary(marginal_effects)
+predicted_probit <- predict(myprobit, type = 'response')
+marginal_effectsP <- margins(myprobit)
+summary(marginal_effectsP) # R rounds to 0
+print(marginal_effectsP, digits = 6) # do this to see actual values
 
 #residuals
 Fitted <- fitted(myprobit)
@@ -84,3 +99,9 @@ ggplot(coup_data, aes(x = 1:249, y = pearson_residuals_probit)) +
   labs(title = "Pearson Residuals from Probit Model",
        x = "Observations", y = "Pearson Residuals") +
   theme_minimal()
+
+#find Cook's Distance (for influential outliers)
+probit_cooksD = cooks.distance(myprobit)
+
+#plotting Cook's Distance (base, need to add clarity and labels)
+plot(probit_cooksD, type = "p", main = "Cook's Distance (Probit)", ylab = "Cook's Distance", xlab = "Observation Index")
