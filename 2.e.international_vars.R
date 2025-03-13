e3421a01c039bb3ccee5207e1b84974e15b59321
thynec <clayton.thyne@uky.edu>
2025-03-13 08:15:44 -0400

    Update 2.e.international_vars.R

--- 2.e.international_vars.R
+++ 2.e.international_vars.R
@@ -5,127 +5,136 @@
 #Building international-related variables
 
 #Priority vars:
-  #trade
-  #cold war dummy
-  #ongoing interstate war
-  #regional contagion
-  #regional dummies
-  #FDI
+#regional dummies - COMPLETED
+#trade
+#cold war dummy - COMPLETED
+#ongoing interstate war
+#regional contagion
+#FDI
+
 #Secondary variables:
-  #foreign military training (McLauchlin/Seymour/Martel J. Peace Research 2022)
-  #international signals
-  #rivalries (Bak/Chavez/Rider J. Conflict Resolution 2019)
-  #defensive alliances (Boutton Intl Studies Quarterly 2019)
-  #peacekeeping (Banini/Powell/Yekple African Security 2020)
+#foreign military training (McLauchlin/Seymour/Martel J. Peace Research 2022)
+#international signals
+#rivalries (Bak/Chavez/Rider J. Conflict Resolution 2019)
+#defensive alliances (Boutton Intl Studies Quarterly 2019)
+#peacekeeping (Banini/Powell/Yekple African Security 2020)
 
 #1. clear all
-  rm(list = ls())
+rm(list = ls())
 #2. set working directory
-  #setwd("~/R/coupcats") # Set working file. 
-  #setwd("C:/Users/clayt/OneDrive - University of Kentucky/elements/current_research/coupcats") #Clay at home
-  #setwd("C:/Users/clthyn2/OneDrive - University of Kentucky/elements/current_research/coupcats") #clay at work
+#setwd("~/R/coupcats") # Set working file. 
+#setwd("C:/Users/clayt/OneDrive - University of Kentucky/elements/current_research/coupcats") #Clay at home
+setwd("C:/Users/clthyn2/OneDrive - University of Kentucky/elements/current_research/coupcats") #clay at work
 #3. install packages
-  #source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/packages.R") 
+#source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/packages.R") 
 #4. load libraries
-  #source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/libraries.R") 
+#source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/libraries.R") 
 #5. build baseline
-  source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/1.building_baseline.R")
+source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/1.building_baseline.R")
+
+#------------------------------------------------------------------------------------------------#
+#add cold war dummy
+#------------------------------------------------------------------------------------------------#  
+
+base_data <- base_data %>%
+  mutate(cold=ifelse(year<=1989, 1, 0))
 
 #------------------------------------------------------------------------------------------------#
 #add regions
 #------------------------------------------------------------------------------------------------#  
 
-  
 #get regions from WDI
-  df <- WDI(indicator = c("CC.EST", "CC.PER.RNK"), country = "all", start = 1996, end = 2023, extra = TRUE)
-  df <- df %>%
-    select(country, region) %>%
-    distinct() %>%
-    left_join(ccodes, by=c("country"))
-  check <- df %>%
-    filter(is.na(ccode))
-  table(check$country) #add Turkey=ccode=640
-  df <- df %>%
-    mutate(ccode=ifelse(country=="Turkiye", 640, ccode))
-  check <- df %>%
-    filter(is.na(ccode))
-  table(check$country) #all good
-  rm(check)
-  df <- df %>%
-    select(-year) %>%
-    distinct()
-  df <- df %>%
-    rename(wdi_region=region) %>%
-    mutate(e_asia_pacific=ifelse(wdi_region=="East Asia & Pacific", 1, 0)) %>%
-    mutate(euro_cent_asia=ifelse(wdi_region=="Europe & Central Asia", 1, 0)) %>%
-    mutate(LA_carrib=ifelse(wdi_region=="Latin America & Caribbean", 1, 0)) %>%
-    mutate(MENA=ifelse(wdi_region=="Middle East & North Africa", 1, 0)) %>%
-    mutate(N_america=ifelse(wdi_region=="North America", 1, 0)) %>%
-    mutate(S_asia=ifelse(wdi_region=="South Asia", 1, 0)) %>%
-    mutate(Sub_africa=ifelse(wdi_region=="Sub-Saharan Africa", 1, 0)) %>%
-    set_variable_labels(
-      e_asia_pacific="East Asia & Pacific",
-      euro_cent_asia="Europe & Central Asia",
-      LA_carrib="Latin America & Caribbean",
-      MENA="Middle East & North Africa",
-      N_america="North America",
-      S_asia="South Asia",
-      Sub_africa="Sub-Saharan Africa") %>%
-    select(-country, -wdi_region)
-  base <- base_data %>%
-    left_join(df, by=c("ccode"))
-  check <- base %>%
-    filter(is.na(e_asia_pacific)) %>%
-    select(country, year) %>%
-    distinct()
-  
-  
-  
-  %>%
-    select(-wdi_region)
-  
-  
-  
-  
-  
-  
-  
-  # 1.2. Cleaning up data. 
-  wdi_data <- wdi_data %>%
-    mutate(year=year+1) %>% #just lagged by adding a year
-    subset(select = c(country, year, CC.EST, CC.PER.RNK, region)) %>% 
-    rename(corruption_est = CC.EST, 
-           corruption_rank = CC.PER.RNK,
-           wdi_region = region) %>%
-    set_variable_labels(
-      corruption_est="Control of Corruption: Estimate; from WDI; t-1",
-      corruption_rank="Control of Corruption: Percentile Rank; from WDI; t-1",
-      wdi_region="regions from WDI"
-    )
-  wdi_data <- wdi_data %>%
-    left_join(ccodes, by = c("year", "country")) 
-  check <- wdi_data %>%
-    filter(if_any(everything(), is.na)) 
-  table(check$country)
-  table(check$year)
-  #check looks fine; missing 3 in normal places due to temporal coverage; other missings are due to small countries or regions
-  rm(check) 
-  
-  # 1.3. Merging into data set. Do this twice. First for corruption by ccode/year; then for region by ccode (region doesn't vary by time)
-  wdi1 <- wdi_data %>%
-    select(-country, -wdi_region)
-  base_data <- base_data %>% 
-    left_join(wdi1, by = c("year", "ccode")) # NAs are unreported years. 
-  wdi2 <- wdi_data %>%
-    select(ccode, wdi_region) %>%
-    distinct()
-  base_data <- base_data %>%
-    left_join(wdi2, by=c("ccode"))
-  rm(wdi_data, wdi1, wdi2)
-  
-  #Add regional dummies
+df <- WDI(indicator = c("CC.EST", "CC.PER.RNK"), country = "all", start = 1960, end = 2023, extra = TRUE)
+df <- df %>%
+  select(country, region) %>%
+  distinct() 
+ccodes2 <- ccodes %>%
+  select(-year) %>%
+  distinct()
+df <- df %>%
+  left_join(ccodes2, by=c("country"))
+rm(ccodes2)
+check <- df %>%
+  filter(is.na(ccode))
+table(check$country) #add Turkey=ccode=640
+df <- df %>%
+  mutate(ccode=ifelse(country=="Turkiye", 640, ccode))
+check <- df %>%
+  filter(is.na(ccode))
+table(check$country) #all good
+rm(check)
+df <- df %>%
+  select(-country) %>%
+  distinct()
+df <- df %>%
+  rename(wdi_region=region) %>%
+  mutate(e_asia_pacific=ifelse(wdi_region=="East Asia & Pacific", 1, 0)) %>%
+  mutate(euro_cent_asia=ifelse(wdi_region=="Europe & Central Asia", 1, 0)) %>%
+  mutate(LA_carrib=ifelse(wdi_region=="Latin America & Caribbean", 1, 0)) %>%
+  mutate(MENA=ifelse(wdi_region=="Middle East & North Africa", 1, 0)) %>%
+  mutate(N_america=ifelse(wdi_region=="North America", 1, 0)) %>%
+  mutate(S_asia=ifelse(wdi_region=="South Asia", 1, 0)) %>%
+  mutate(Sub_africa=ifelse(wdi_region=="Sub-Saharan Africa", 1, 0)) %>%
+  set_variable_labels(
+    e_asia_pacific="East Asia & Pacific",
+    euro_cent_asia="Europe & Central Asia",
+    LA_carrib="Latin America & Caribbean",
+    MENA="Middle East & North Africa",
+    N_america="North America",
+    S_asia="South Asia",
+    Sub_africa="Sub-Saharan Africa") %>%
+  select(-wdi_region)
+base <- base_data %>%
+  left_join(df, by=c("ccode"))
+check <- base %>%
+  filter(is.na(MENA))
+table(check$country) #got problems; WDI not recognizing old names; easy to fix manually
+rm(check)
+#fix missing
+base <- base %>%
+  mutate(euro_cent_asia=ifelse(country=="Czech Republic", 1, euro_cent_asia)) %>%
+  mutate(euro_cent_asia=ifelse(country=="Czechoslovakia", 1, euro_cent_asia)) %>%
+  mutate(euro_cent_asia=ifelse(country=="German Democratic Republic", 1, euro_cent_asia)) %>%
+  mutate(euro_cent_asia=ifelse(country=="German Federal Republic", 1, euro_cent_asia)) %>%
+  mutate(e_asia_pacific=ifelse(country=="Republic of Vietnam", 1, e_asia_pacific)) %>%
+  mutate(e_asia_pacific=ifelse(country=="Taiwan", 1, e_asia_pacific)) %>%
+  mutate(e_asia_pacific=ifelse(country=="Vietnam", 1, e_asia_pacific)) %>%
+  mutate(Sub_africa=ifelse(country=="Zanzibar", 1, Sub_africa)) %>%
+  mutate(MENA=ifelse(country=="Yemen Arab Republic", 1, MENA)) %>%
+  mutate(MENA=ifelse(country=="Yemen People's Republic", 1, MENA))
+check <- base %>%
+  filter(is.na(e_asia_pacific)) %>%
+  filter(is.na(MENA)) %>%
+  filter(is.na(euro_cent_asia)) %>%
+  filter(is.na(LA_carrib)) %>%
+  filter(is.na(N_america)) %>%
+  filter(is.na(S_asia)) %>%
+  filter(is.na(Sub_africa))
+table(check$country) #looks good
+rm(check)
+base <- base %>%
+  mutate(across(everything(), ~ replace_na(.x, 0)))
+base_data <- base
+rm(base, df)
 
-  
+###############################################################################################
+#Checked through above and ready to produce .csv and upload to github
+#clean up if needed and export
+write.csv(base_data, gzfile("2.e.base_data.csv.gz"), row.names = FALSE)
+#Now push push the file that was just written to the working directory to github
+###############################################################################################  
+
+
+
+
+#------------------------------------------------------------------------------------------------#
+#add trade
+#------------------------------------------------------------------------------------------------#  
+
+#Start with COW
+
+
+
 
 
 
@@ -216,7 +225,6 @@ copdab <-  copdab %>%
 write.csv(base_data, gzfile("2.e.base_data.csv.gz"), row.names = FALSE)
 #Now push push the file that was just written to the working directory to github
 ###############################################################################################  
-
 
 
 
