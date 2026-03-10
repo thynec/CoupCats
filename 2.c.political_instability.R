cf08d250afb0bc33642f40f9de9991de8a022e60
thynec <clayton.thyne@uky.edu>
2026-03-10 17:27:58 -0400

    Integrate ACLED protest data and update data processing
    Added protest data from ACLED and merged it with base_data. Updated data cleaning and reshaping processes for political stability and mass mobilization.

--- 2.c.political_instability.R
+++ 2.c.political_instability.R
@@ -9,60 +9,13 @@ rm(list = ls())
 #2. set working directory
 #setwd("~/R/coupcats") # Set working file. 
 #setwd("C:/Users/clayt/OneDrive - University of Kentucky/elements/current_research/coupcats") #Clay at home
-setwd("C:/Users/clthyn2/OneDrive - University of Kentucky/elements/current_research/coupcats") #clay at work
+#setwd("C:/Users/clthyn2/OneDrive - University of Kentucky/elements/current_research/coupcats") #clay at work
 #3. install packages
 #source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/packages.R") 
 #4. load libraries
 #source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/libraries.R") 
 #5. build baseline
 source("https://raw.githubusercontent.com/thynec/CoupCats/refs/heads/main/1.building_baseline.R")
-
-#------------------------------------------------------------------------------------------------#
-#civil wars from UCDP/PRIO ACD; https://ucdp.uu.se/downloads/index.html#armedconflict
-#------------------------------------------------------------------------------------------------#  
-
-#bring in data
-url <- "https://ucdp.uu.se/downloads/ucdpprio/ucdp-prio-acd-241-xlsx.zip"
-download.file(url, "data.zip")
-unzip("data.zip", exdir="data")
-unlink("data.zip")
-cw <- read_excel("data/UcdpPrioConflict_v24_1.xlsx")
-unlink("data", recursive=TRUE)
-rm(url)  
-
-#clean data; only type 3 and 4 CWs
-cw <- cw %>%
-  filter(type_of_conflict=="3" | type_of_conflict=="4") %>%
-  mutate(loc = as.numeric(gwno_loc)) %>%
-  mutate(year = as.numeric(year)) %>%
-  filter(loc!=2) %>% #US in civil war is miscoded
-  select(location, year, loc) %>%
-  rename(country=location) 
-cw <- cw %>%
-  left_join(ccodes, by=c("country", "year"))
-cw <- cw %>%
-  mutate(ccode=ifelse(country=="Madagascar (Malagasy)", 580, ccode))
-cw <- cw %>%
-  mutate(year=year+1) %>% #just lagged
-  rename(mcountry=country) %>%
-  select(-loc) %>%
-  mutate(cw=1) %>%
-  distinct() 
-summary(cw$year) #fill missing years from 1947-2024
-#expand for 2025; assuming ongoing CWs in 2024 continue to 2025
-cw <- cw %>%
-  mutate(expand=ifelse(year==2024, 2, 1)) %>%
-  uncount(expand) %>%
-  arrange(ccode, year) %>%
-  mutate(year=ifelse(year==2024 & lag(year)==2024 & ccode==lag(ccode), 2025, year))
-#merge into base
-base_data <- base_data %>%
-  left_join(cw, by=c("ccode", "year"))
-rm(cw)
-base_data <- base_data %>%
-  mutate(cw=ifelse(is.na(cw) & year>=1947 & year<=2025, 0, cw)) %>%
-  set_variable_labels(cw="3-4 types from UCDP, t-1") %>%
-  select(-mcountry)
 
 # -------------------------- Political Stability (From World Bank) ----------------------------- #
 #1.1 Getting the data
@@ -116,15 +69,13 @@ vdem_data <- vdem_data %>% # Merging in ccodes.
   left_join(ccodes, by = c("year", "country")) 
 vdem_data <- vdem_data %>%
   filter(!is.na(ccode)) # No Republic of Vietnam (1950-76), Afghanistan (1950-2000), Guinea-Bissau (1950-2005), Laos (2024), Iceland (1950-2019)
-vdem_data <- vdem_data %>%
-  mutate(expand=ifelse(year==2024, 2, 1)) %>%
-  uncount(expand) %>%
-  arrange(ccode, year) %>%
-  mutate(year=ifelse(year==2024 & lag(year)==2024 & ccode==lag(ccode), 2025, year)) %>%
-  mutate(mobilization=ifelse(year==2025, NA, mobilization)) %>%
-  mutate(mobil_conc=ifelse(year==2025, NA, mobil_conc))
 
 # Merging into data set. 
+check <- vdem_data %>%
+  arrange(ccode, year) %>%
+  filter(ccode==lag(ccode) & year==lag(year)) #0 so we're good
+rm(check)
+
 vdem_data <- vdem_data %>%
   rename(mcountry = country)
 base_data <- base_data %>%
@@ -133,9 +84,144 @@ rm(vdem_data)
 base_data <- base_data %>%
   select(-mcountry)
 
+#------------------------------------------------------------------------------------------------#  
+#add in protest data from acled
+#------------------------------------------------------------------------------------------------#  
+
+#download 'Number of political violence events by country-month-year' from https://acleddata.com/aggregated/number-political-violence-events-country-month-year
+#note that need to log in to download these, so downloaded on 03/10/26 and put on github; will need to grab data every time we need to update
+url <- "https://github.com/thynec/CoupCats/raw/refs/heads/data/number_of_political_violence_events_by_country-month-year_as-of-27Feb2026.xlsx"
+destfile <- "number_of_political_violence_events_by_country_month_year_as_of_27Feb2026.xlsx"
+curl::curl_download(url, destfile)
+acled <- read_excel(destfile)
+rm(destfile, url)
+acled <- acled %>%
+  rename(country=COUNTRY,
+         month=MONTH,
+         year=YEAR,
+         acled=EVENTS) %>%
+  set_variable_labels(acled="pol violence events by month") %>% 
+  mutate(month=match(month, month.name)) %>%
+  mutate(date = ymd(paste(year, month, "01"))) %>%
+  mutate(date=date %m+% months(1)) %>%
+  select(-month, -year) %>%
+  mutate(year=year(date)) %>%
+  mutate(month=month(date)) %>%
+  select(-date)
+acled <- acled %>%
+  left_join(ccodes, by=c("country", "year"))
+check <- acled %>%
+  filter(is.na(ccode)) %>%
+  select(country) %>%
+  distinct()
+#all missing ccodes are weird ones; fine to ignore
+rm(check)
+acled <- acled %>%
+  select(-country)
+base_data <- base_data %>%
+  left_join(acled, by=c("ccode", "year", "month"))
+rm(acled)
+
+#compare acled to vdem
+check <- base_data %>%
+  mutate(acled=log(acled+100))
+cor(check %>% 
+      select(mobilization, mobil_conc, acled),
+    use = "complete.obs")
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------#
+#civil wars from UCDP/PRIO ACD; https://ucdp.uu.se/downloads/index.html#armedconflict
+#------------------------------------------------------------------------------------------------#  
+
+#bring in data
+url <- "https://ucdp.uu.se/downloads/ucdpprio/ucdp-prio-acd-241-xlsx.zip"
+download.file(url, "data.zip")
+unzip("data.zip", exdir="data")
+unlink("data.zip")
+cw <- read_excel("data/UcdpPrioConflict_v24_1.xlsx")
+unlink("data", recursive=TRUE)
+rm(url)  
+
+#clean data; only type 3 and 4 CWs
+cw <- cw %>%
+  filter(type_of_conflict=="3" | type_of_conflict=="4") %>%
+  mutate(loc = as.numeric(gwno_loc)) %>%
+  mutate(year = as.numeric(year)) %>%
+  filter(loc!=2) %>% #US in civil war is miscoded
+  select(location, year, loc) %>%
+  rename(country=location) 
+cw <- cw %>%
+  left_join(ccodes, by=c("country", "year"))
+cw <- cw %>%
+  mutate(ccode=ifelse(country=="Madagascar (Malagasy)", 580, ccode))
+cw <- cw %>%
+  mutate(year=year+1) %>% #just lagged
+  rename(mcountry=country) %>%
+  select(-loc) %>%
+  mutate(cw=1) %>%
+  distinct() 
+summary(cw$year) #fill missing years from 1947-2024
+#expand for 2025; assuming ongoing CWs in 2024 continue to 2025
+cw <- cw %>%
+  mutate(expand=ifelse(year==2024, 2, 1)) %>%
+  uncount(expand) %>%
+  arrange(ccode, year) %>%
+  mutate(year=ifelse(year==2024 & lag(year)==2024 & ccode==lag(ccode), 2025, year))
+#merge into base
+base_data <- base_data %>%
+  left_join(cw, by=c("ccode", "year"))
+rm(cw)
+base_data <- base_data %>%
+  mutate(cw=ifelse(is.na(cw) & year>=1947 & year<=2025, 0, cw)) %>%
+  set_variable_labels(cw="3-4 types from UCDP, t-1") %>%
+  select(-mcountry)
+
+
+
+
+
+
+
+
+
+
+
 ###############################################################################################
 #Checked through above and ready to produce .csv and upload to github
 #clean up if needed and export
 write.csv(base_data, gzfile("2.c.base_data.csv.gz"), row.names = FALSE)
 #Now push push the file that was just written to the working directory to github
 ###############################################################################################  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
